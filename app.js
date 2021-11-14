a1afbb0e46407002b05ce3fe80c647f109eb8bc3
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-14 18:28:36 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  const message = req.body;
+  return res.send(message);
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
