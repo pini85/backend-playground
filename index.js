a78112117e00ca5696ef3c95113972bc0a78e1c7
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-15 11:53:57 +0200

    a

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname)));
+
 app.get("/test", (req, res) => {
   console.log("hi");
   const message = req.body;
