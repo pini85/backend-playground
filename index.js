c1ba3d583a655659b63298b82181898f15f8c329
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-16 15:08:13 +0200

    a

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const wss = new WebSocket.Server({ server: server });
 
 wss.on("connection", function connection(ws) {
   console.log("A new client Connected!");
-  ws.send("Welcome New Client!");
 
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
