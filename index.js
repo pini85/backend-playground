3d5cf3642c86a361143d5ad7ccc46c05c76d897b
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-16 11:35:39 +0200

    a

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const server = require("https").createServer(app);
+const WebSocket = require("ws");
 
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname)));
-
+const wss = new WebSocket.Server({ server });
+wss.on("connection", function connection(ws) {
+  console.log("new client connected");
+  ws.send("welcome new client");
+  ws.on("message", function incoming(message) {
+    console.log("message recived", message);
+    ws.send("got your message its" + message);
+  });
+});
 app.post("/test", (req, res) => {
   console.log("hello");
   const message = req.body;
@@ -16,7 +25,7 @@ app.post("/test", (req, res) => {
 app.get("*", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
