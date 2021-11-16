1167361cd5e66712c2565f93f833f1e81ff4cb4f
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2021-11-16 13:18:12 +0200

    testing web sockets

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const server = require("https").createServer(app);
+const server = require("http").createServer(app);
 const WebSocket = require("ws");
 
-app.use(cors());
-app.use(express.json());
+const wss = new WebSocket.Server({ server: server });
 
-const wss = new WebSocket.Server({ server });
 wss.on("connection", function connection(ws) {
-  console.log("new client connected");
-  ws.send("welcome new client");
+  console.log("A new client Connected!");
+  ws.send("Welcome New Client!");
+
   ws.on("message", function incoming(message) {
-    console.log("message recived", message);
-    ws.send("got your message its" + message);
+    console.log("received: %s", message);
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
   });
 });
-app.post("/test", (req, res) => {
-  console.log("hello");
-  const message = req.body;
-  return res.send(message);
-});
 
-app.get("*", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
-});
+app.get("/", (req, res) => res.send("Hello World!"));
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Lisening on port :${PORT}`));
